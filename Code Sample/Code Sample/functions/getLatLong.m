543ab90f5f83777b266d14bd5141f513ec3a466a
levi-lucy <levi.lucy@ndsu.edu>
2018-03-02 09:31:42 -0600

    Delete getLatLong.m

--- Code Sample/Code Sample/functions/getLatLong.m
+++
@@ -1,6 +0,0 @@
-%%
-%retrieves the lat long coordinates of a csr code
-function latLong = getLatLong(locTable, csrCode) 
-    ind = findPlace(locTable(:,1), csrCode);
-    latLong = locTable(ind,[2 3 4]);
-end

e409884d61d064a795b0d335b4d6c3cc27c31349
levi-lucy <levi.lucy@ndsu.edu>
2018-03-02 09:31:49 -0600

    Delete getNames.m

--- Code Sample/Code Sample/functions/getNames.m
+++
@@ -1,9 +0,0 @@
-%%
-%Function to get a location's name based on the csr code. csrList is the
-%list of csr codes, csrCode is the csr code to lookup, namesList is the
-%list of location names. the i-th index of csrList corresponds to the i-th
-%index of namesList. 
-function name = getNames(csrList, csrCode, namesList)
-            ind = findPlace(csrList,csrCode);
-            name = namesList(ind,1);
-end
